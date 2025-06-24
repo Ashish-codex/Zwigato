ee5652d7507a985f73103da0e3bbbf8622b5decb
Ashish-codex <ashish.prajapati16200@gmail.com>
2025-06-24 15:08:45 +0530

    [New :] Project setup

---
+++ Zwigato/Features/HomeScreen/View/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  Zwigato
+//
+//  Created by Ashish Prajapati on 24/06/25.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
---
+++ Zwigato/Features/HomeScreen/ViewModel/HomeScreenViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeScreenViewModel.swift
+//  Zwigato
+//
+//  Created by Ashish Prajapati on 24/06/25.
+//
+
+import Foundation
+
+class HomeScreenViewModel{
+    
+}
