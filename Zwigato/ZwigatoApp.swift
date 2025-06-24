c454332f80b5138a6b5ca2177f830de58677d929
Ashish Prajapati <88182121+Ashish-codex@users.noreply.github.com>
2025-06-24 14:48:48 +0530

    Initial Commit

---
+++ Zwigato/ZwigatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZwigatoApp.swift
+//  Zwigato
+//
+//  Created by Ashish Prajapati on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ZwigatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
